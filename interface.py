a645adb1b7afb5abeadab0ae24928ba0f01d75a3
CristianAlarcon <48411410+CristianAlarcon@users.noreply.github.com>
2023-07-25 14:29:15 +0200

    Update interface.py

--- interface.py
+++ interface.py
@@ -148,7 +148,7 @@ if districte_censal_str != '000':
 
     def obtenir_preu_per_zona(df, zona):
         try:
-            fila = df.loc[df['zona'] == zona]
+            fila = df.loc[df['Zona'] == zona]
             precio_m2 = fila['precioM2'].values[0]
             return precio_m2
         except IndexError:
